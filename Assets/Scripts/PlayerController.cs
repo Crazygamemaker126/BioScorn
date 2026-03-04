90fd652b966e10e7f75dfaf07b4df530229ed7bc
Crazygamemaker126 <jghollowelliv@gmail.com>
2026-03-04 16:29:54 -0600

    Hovering/Player Movement
    This is a rudimentary player controller with a functional hovering mechanic. I still need to work in obstacle detection

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,93 @@
+using UnityEngine;
+using UnityEngine.Events;
+using System;
+using System.Collections;
+
+[RequireComponent (typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 4f;
+    public float jumpForce = 5f;
+    public LayerMask groundMask;
+    public float currentmaxLinVel;
+
+    public float hoveringDuration = 5;
+    public float hoveringTimer;
+    public float currentHoverTimeLeft;
+    
+
+    private Rigidbody rb;
+    [SerializeField] private bool isGrounded;
+    [SerializeField] private bool isHovering;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody> ();
+        rb.freezeRotation = true;
+        currentmaxLinVel = rb.maxLinearVelocity;
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+
+        if(Input.GetButtonDown("Jump") && !isGrounded && transform.position.y > 0) 
+        {
+            Hover();
+            hoveringTimer = hoveringDuration;
+           
+            isHovering = true;
+           
+
+        }
+
+        if (isHovering)
+        {
+           
+            if (hoveringTimer <= 0)
+            {
+                StopHover();
+                isHovering = false;
+
+            }
+        }
+
+        hoveringTimer -= Time.deltaTime;
+    }
+
+    private void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 move = (transform.forward * v + transform.right * h) * moveSpeed;
+        Vector3 newVelocity = new Vector3(move.x, rb.linearVelocity.y, move.z);
+        rb.linearVelocity = newVelocity;
+
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, 1.1f, groundMask);
+    }
+
+    public void Hover() 
+    {
+        float currentYPosition = transform.position.y;
+        
+        rb.useGravity = false;
+        rb.maxLinearVelocity = currentYPosition;
+
+    }
+
+    public void StopHover() 
+    {
+        
+        rb.useGravity = true;
+        rb.maxLinearVelocity = currentmaxLinVel;
+    }
+
+    private void OnDrawGizmos()
+    {
+        
+    }
+}
