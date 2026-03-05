089c5432603ac80b2f6b482f7c4c0045c0020fa3
Crazygamemaker126 <jghollowelliv@gmail.com>
2026-03-05 17:33:24 -0600

    Updated Hover mechanic for playability
    Hover mechanic now will regen while player is not hovering, and must refill completely before player can hover again.
    
    I also added a deathzone underneath the holes Nate made, and have also included a Win sphere. Currently, the deathzone will allow players to reload the scene, but the winsphere will not.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 using UnityEngine.Events;
 using System;
 using System.Collections;
+using UnityEngine.UI;
+using System.IO;
 
 [RequireComponent (typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
@@ -12,47 +14,110 @@ public class PlayerController : MonoBehaviour
     public LayerMask slopeMask;
     public float currentmaxLinVel;
 
-    public float hoveringDuration = 5;
+    public float maxHoverDuration = 5f;
     public float hoveringTimer;
     public float currentHoverTimeLeft; // will use to beef up hover mechanic as deployment ensues
+    public float slopeJumpOffset = 3f;
+    public float originalAngularDamping;
 
     public float groundDrag = 5f;
     public float airDrag = 0f;
     public float slopeDrag = 10f;
+
+    public Slider hoverTimerSlider;
     
 
     private Rigidbody rb;
     [SerializeField] private bool isGrounded;
     [SerializeField] private bool isHovering;
     [SerializeField] private bool onSlope;
+    [SerializeField] private bool jumpingFromSlope;
+    [SerializeField] private bool canHover = true;
+
+    private Coroutine hoverCooldownCo;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody> ();
         rb.freezeRotation = true;
         currentmaxLinVel = rb.maxLinearVelocity;
+        hoverTimerSlider.maxValue = maxHoverDuration;
+        hoverTimerSlider.value = hoveringTimer;
+        originalAngularDamping = rb.angularDamping;
+        hoveringTimer = maxHoverDuration;
+        currentHoverTimeLeft = maxHoverDuration;
+        canHover = true;
+
+        UpdateHoverTimer();
+        
+
     }
 
     private void Update()
     {
+        UpdateHoverTimer();
+
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
-            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            if (!onSlope)
+            {
+                rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            }
+            else 
+            {
+                rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+                jumpingFromSlope = true;
+            }
         }
 
-        if(Input.GetButtonDown("Jump") && !isGrounded && transform.position.y > 0) 
+        if(Input.GetButtonDown("Jump") && !isGrounded && transform.position.y > 0 && hoverCooldownCo == null) 
         {
-            Hover();
-            hoveringTimer = hoveringDuration;
-           
-            isHovering = true;
-           
+         
 
+            if (!isHovering && canHover)
+            { 
+                StartHover();
+                if (!jumpingFromSlope)
+                {
+                    float currentYPosition = transform.position.y;
+
+                    rb.useGravity = false;
+                    rb.maxLinearVelocity = currentYPosition;
+                }
+                else
+                {
+                    float offsetYPosition = transform.position.y + slopeJumpOffset;
+
+                    float offsetAngDrag = (rb.angularDamping * airDrag) - (jumpForce - transform.position.y);
+                    rb.AddForce(Vector3.up, ForceMode.Impulse);
+                    rb.useGravity = false;
+                    rb.maxLinearVelocity = offsetYPosition;
+                    rb.angularDamping = offsetAngDrag;
+                    jumpingFromSlope = false;
+                }
+                
+                
+
+               
+
+            }
+            else if(Input.GetButtonDown("Jump") && isHovering)
+            {
+                StopHover();
+                isHovering = false;
+            }
+
+        }
+
+        if (isGrounded) 
+        {
+            isHovering = false;
         }
 
         if (isHovering)
         {
-           
+            hoveringTimer -= Time.deltaTime;
+
             if (hoveringTimer <= 0)
             {
                 StopHover();
@@ -60,8 +125,15 @@ public class PlayerController : MonoBehaviour
 
             }
         }
+        else 
+        {
+            hoveringTimer += Time.deltaTime;
+            hoveringTimer = Mathf.Clamp(hoveringTimer, 0, maxHoverDuration);
+            
+        }
 
-        hoveringTimer -= Time.deltaTime;
+        
+       
     }
 
     private void FixedUpdate()
@@ -91,20 +163,71 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    public void Hover() 
+    public void StartHover() 
     {
-        float currentYPosition = transform.position.y;
-        
-        rb.useGravity = false;
-        rb.maxLinearVelocity = currentYPosition;
+        isHovering = true;
+        canHover = false;
+
+        if (currentHoverTimeLeft < 0)
+        {
+            StartCoroutine(HoverCooldownCo());
+        }
+
 
     }
 
     public void StopHover() 
     {
+        canHover = false;
+        if (!jumpingFromSlope)
+        {
+            rb.useGravity = true;
+
+            rb.maxLinearVelocity = currentmaxLinVel;
+        }
+        else 
+        {
+            rb.useGravity = true;
+
+            rb.maxLinearVelocity = currentmaxLinVel;
+            jumpingFromSlope = false;
+            rb.angularDamping = originalAngularDamping;
+        }
         
-        rb.useGravity = true;
-        rb.maxLinearVelocity = currentmaxLinVel;
+        if (hoverCooldownCo != null)
+        {
+            StopCoroutine(hoverCooldownCo);
+
+        }
+        else
+        {
+            hoverCooldownCo = StartCoroutine(HoverCooldownCo());
+        }
+    }
+
+    public void UpdateHoverTimer() 
+    {
+        hoverTimerSlider.value = hoveringTimer;
+
+    }
+
+   
+
+    public IEnumerator HoverCooldownCo() 
+    {
+        hoveringTimer += Time.deltaTime;
+        canHover = false;
+        isHovering = false;
+        Debug.Log("Cool those jets");
+
+        
+        yield return new WaitForSeconds(maxHoverDuration - hoveringTimer);
+
+        if(hoveringTimer >= maxHoverDuration)
+       canHover = true;
+        hoverCooldownCo = null;
+
+
     }
 
     private void OnDrawGizmos()
