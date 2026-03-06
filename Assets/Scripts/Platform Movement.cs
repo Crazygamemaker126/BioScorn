6bae6459d08909cb73476a7a8713cb9b8c9c7c6b
Nate621234 <ptownnatedogg@gmail.com>
2026-03-05 21:29:14 -0600

    Moving Platform
    I added a script for vertical and horizontal movement for a base idea of the moving platform concept. You do have to move with  the platform as its moving because I couldn't get the player to sitck onto it.

---
+++ Assets/Scripts/Platform Movement.cs
@@ -0,0 +1,32 @@
+
+using UnityEngine;
+
+
+public class MovingPlatform : MonoBehaviour
+{
+    public float speed = 2f;
+    public float distance = 3f;
+
+    public bool moveHorizontal;
+    public bool moveVertical;
+
+    private Vector3 startPosition;
+
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    void Update()
+    {
+        if (moveHorizontal)
+        {
+            transform.position = startPosition + new Vector3(Mathf.Sin(Time.time * speed) * distance, 0, 0);
+        }
+
+        if (moveVertical)
+        {
+            transform.position = startPosition + new Vector3(0, Mathf.Sin(Time.time * speed) * distance, 0);
+        }
+    }
+}
